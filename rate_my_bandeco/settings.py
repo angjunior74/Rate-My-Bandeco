9490862418745aecfd965d377a2069d3680342b0
alexmegaprime-gif <alexmegaprime@gmail.com>
2025-11-30 19:13:57 -0300

    Update settings.py

--- rate_my_bandeco/settings.py
+++ rate_my_bandeco/settings.py
@@ -128,11 +128,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = 'ratemybandeco@gmail.com'  
-#EMAIL_HOST_PASSWORD = 'mpxulahcdjhtvyxc'   
-#DEFAULT_FROM_EMAIL = 'ratemybandeco@gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "apikey"   # sempre essa palavra
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+DEFAULT_FROM_EMAIL = "nao-responda@seudominio.com"
+
